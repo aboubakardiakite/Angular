5761179732ef4a51d42101e67f93791d82ccf649
diakite <aboubakarsiriki.diakite.etu@univ-lille.fr>
2023-03-27 19:10:38 +0200

    add interface of cocktail

--- cocltails/src/app/app.component.ts
+++ cocltails/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Cocktails } from './interfaces/cocktail.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'cocltails';
+  cocktails: Cocktails = {
+    name: 'Mojiro',
+    img: 'https://www.destinationcocktails.fr/wp-content/uploads/2019/11/cocktail-mojito-2.jpg',
+    description:
+      'Ce cocktail cubain à base de rhum, dont la réputation n’est plus à faire, est assez facile à préparer et est toujours apprécié. Délectez-vous de sa fraîcheur et de ses arômes de menthe et de citron vert… Vous ravirez les papilles de vos convives à coup sûr lors de vos soirées ou apéritifs !',
+  };
 }
